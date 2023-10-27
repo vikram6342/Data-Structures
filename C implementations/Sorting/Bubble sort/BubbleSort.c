56f40e5fd5c63898dbfc5bd232ea2e33d979db29
Adithya <adithyanarayanmk@gmail.com>
2023-10-27 12:29:04 +0530

    added gitignore file for c implementations

---
+++ C implementations/Sorting/Bubble sort/BubbleSort.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>  
+
+void bubblesort(int* arr,int size);
+void display(int* arr,int size);
+void main(){
+    int size;
+    printf("Enter the array size");
+    scanf("%d",&size);
+    int arr[size];
+    for(int i=0;i<size;i++){
+        printf("Enter the %dth data : ",i+1);
+        scanf("%d",&arr[i]);
+    }
+    bubblesort(arr,size);
+    display(arr,size);
+}
+
+void display(int* arr,int size){
+    for(int i=0;i<size;i++)
+        printf("%d ",arr[i]);
+}
+
+void bubblesort(int* arr,int size){
+    for(int i = 0;i<size;i++){
+        bool flag = false;
+        for(int j=0;j<size;j++){
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                flag = true;
+            }
+        }
+        if(flag==false)
+            break;
+    }        
+}
--- JAVA Implementations/src/Queue/QueueMain.java
+++ JAVA Implementations/src/Queue/QueueMain.java
@@ -2,36 +2,36 @@ package Queue;
 
 public class QueueMain {
     public static void main(String[] args) throws Exception {
-        CustomQueue queue = new CustomQueue(5);
-        queue.insert(1);
-        queue.insert(3);
-        queue.insert(145);
-        queue.insert(123);
-        queue.insert(6);
-
-        queue.display();
-        System.out.println(queue.front());
-        System.out.println(queue.remove());
-        queue.display();
-
-        Queue queue1 = new Queue(5);
-        queue1.enqueue(1);
-        queue1.enqueue(2);
-        queue1.enqueue(3);
-        queue1.enqueue(4);
-        queue1.enqueue(5);
-        System.out.println("Current Queue Size:" + queue1.size());
-        System.out.println(queue1.dequeue());
-        System.out.println(queue1.dequeue());
-        System.out.println(queue1.dequeue());
-        System.out.println(queue1.dequeue());
-        System.out.println(queue1.dequeue());
-        System.out.println("Current Queue Size:"+ queue1.size());
+//        CustomQueue queue = new CustomQueue(5);
+//        queue.insert(1);
+//        queue.insert(3);
+//        queue.insert(145);
+//        queue.insert(123);
+//        queue.insert(6);
+//
+//        queue.display();
+//        System.out.println(queue.front());
+//        System.out.println(queue.remove());
+//        queue.display();
+//
+//        Queue queue1 = new Queue(5);
+//        queue1.enqueue(1);
+//        queue1.enqueue(2);
+//        queue1.enqueue(3);
+//        queue1.enqueue(4);
+//        queue1.enqueue(5);
+//        System.out.println("Current Queue Size:" + queue1.size());
+//        System.out.println(queue1.dequeue());
+//        System.out.println(queue1.dequeue());
+//        System.out.println(queue1.dequeue());
+//        System.out.println(queue1.dequeue());
+//        System.out.println(queue1.dequeue());
+//        System.out.println("Current Queue Size:"+ queue1.size());
         //here observe that the queue is empty but i can't insert a item
         //So,to optimize it we use circular queue.
-        queue1.enqueue(6);
+      //  queue1.enqueue(6);
         CircularQueue circularQueue= new CircularQueue(5);
-        DynamicCircularQueue dynamicCircularQueue = new DynamicCircularQueue(5);
+       // DynamicCircularQueue dynamicCircularQueue = new DynamicCircularQueue(5);
         circularQueue.insert(3);
         circularQueue.insert(6);
         circularQueue.insert(5);
@@ -39,14 +39,14 @@ public class QueueMain {
         circularQueue.insert(1);
 
         circularQueue.display();
-        circularQueue.insert(55);
-        System.out.println(circularQueue.remove());
-        circularQueue.insert(133);
-        circularQueue.display();
-
-        System.out.println(queue.remove());
-        circularQueue.insert(99);
-        circularQueue.display();
+//        circularQueue.insert(55);
+//        System.out.println(circularQueue.remove());
+//        circularQueue.insert(133);
+//        circularQueue.display();
+//
+//        System.out.println(circularQueue.remove());
+//        circularQueue.insert(99);
+//        circularQueue.display();
 
     }
 }
